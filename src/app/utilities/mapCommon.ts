7ef19f7a8ccde7407ebd5479aaac79627fa7ab9c
umitakbas82 <umit.akbas@hotmail.com.tr>
2025-01-16 17:37:50 +0300

    exrta layer added

--- src/app/utilities/mapCommon.ts
+++ src/app/utilities/mapCommon.ts
@@ -1,4 +1,4 @@
-import { environment } from "../environments/environment";
+
 import { Basemap } from "../models/basemap";
 
 const maptilerKey:(string)="12z1DUufFp494sL7fgKU"
@@ -6,15 +6,19 @@ const maptilerKey:(string)="12z1DUufFp494sL7fgKU"
 
 export const basemapList:Basemap[]=[
 {
-    url:`https://api.maptiler.com/maps/topo-v2/style.json?key=${maptilerKey}`,
+    url:`https://api.maptiler.com/maps/streets-v2/style.json?key=${maptilerKey}`,
     image:"/assets/img/basic-v2.png",
 },
 {
     url:`https://api.maptiler.com/maps/satellite/style.json?key=${maptilerKey}`,
     image:"assets/img/satelitte.png",
 },
+// {
+//     url:`https://api.maptiler.com/tiles/terrain-quantized-mesh-v2/{z}/{x}/{y}.quantized-mesh-1.0?key=12z1DUufFp494sL7fgKU`,
+//     image:"assets/img/topo-v2.png",
+// },
 {
-    url:`https://api.maptiler.com/maps/basic-v2/style.json?key=${maptilerKey}`,
+    url:`https://api.maptiler.com/maps/topo-v2/style.json?key=${maptilerKey}`,
     image:"assets/img/topo-v2.png",
 }
 ]
